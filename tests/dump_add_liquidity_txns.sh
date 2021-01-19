d7bce1a8bc673a51ecda0a0960257342fa3c46ad
Haardik <hhaardik@uwaterloo.ca>
2021-01-19 16:43:12 -0500

    update dryrun dump format to json

--- tests/dump_add_liquidity_txns.sh
+++ tests/dump_add_liquidity_txns.sh
@@ -1,10 +1,10 @@
-export VALIDATOR_INDEX="13605355"
-export MANAGER_INDEX="13605356"
-export TOKEN1_INDEX=""
-export TOKEN2_INDEX=""
-export LIQUIDITY_TOKEN_INDEX=""
-export ESCROW_ADDRESS=""
-export TEST_ACCOUNT_ADDRESS=""
+export VALIDATOR_INDEX="13616699"
+export MANAGER_INDEX="13616702"
+export TOKEN1_INDEX="13616706"
+export TOKEN2_INDEX="13616707"
+export LIQUIDITY_TOKEN_INDEX="13616714"
+export ESCROW_ADDRESS="AUNTAWAYVOG2WTNKVMTM7XTTZRJYPUYESH3GZT4B23YMPY6CNB55KWHLUM"
+export TEST_ACCOUNT_ADDRESS="EFU57I3AX2473QAKM2E4QS7KEMAXOISYY62PE36RKNDJ7RR4X2JJDUFZAQ"
 
 ./goal app call --app-id ${VALIDATOR_INDEX} --app-arg "str:a" --app-arg "str:5" --from ${TEST_ACCOUNT_ADDRESS} --app-account ${ESCROW_ADDRESS} --app-account ${ESCROW_ADDRESS} --foreign-asset ${LIQUIDITY_TOKEN_INDEX} --out=txn1.tx
 
@@ -26,4 +26,4 @@ cat txn1.tx txn2.tx txn3.tx txn4.tx > combinedtxns.tx
 
 cat signout-0.tx signout-1.tx signout-2.tx signout-3.tx > signout.tx
 
-./goal clerk dryrun -t signout.tx --dryrun-dump -o add_liq_dr.msgp
+./goal clerk dryrun -t signout.tx --dryrun-dump --dryrun-dump-format json -o add_liq_dr.msgp

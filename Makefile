f62353c937456c3c3a5b78d922a79ce1e1d92d46
Haardik <hhaardik@uwaterloo.ca>
2021-01-19 12:03:48 -0500

    add add_liquidity_test

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 deploy: venv/bin/activate
 	. ./config.sh; . venv/bin/activate; python3 deploy.py
 
+add_liq_test: venv/bin/activate
+	. ./config.sh; . venv/bin/activate; python3 tests/add_liquidity_test.py
+
 venv/bin/activate: requirements.txt
 	rm -rf venv; python3 -m venv venv; . venv/bin/activate; python3 -m pip install -r requirements.txt
--- tests/add_liquidity_test.py
+++ tests/add_liquidity_test.py
@@ -1,5 +1,6 @@
 import os
 import pytest
+import base64
 
 from algosdk.v2client import algod, indexer
 from algosdk import mnemonic, account
@@ -12,13 +13,15 @@ INDEXER_TOKEN = os.environ['INDEXER_TOKEN']
 TEST_ACCOUNT_PRIVATE_KEY = mnemonic.to_private_key(os.environ['TEST_ACCOUNT_PRIVATE_KEY'])
 TEST_ACCOUNT_ADDRESS = account.address_from_private_key(TEST_ACCOUNT_PRIVATE_KEY)
 
-VALIDATOR_ADDRESS = os.environ['VALIDATOR_ADDRESS']
-MANAGER_ADDRESS = os.environ['MANAGER_ADDRESS']
 ESCROW_ADDRESS = os.environ['ESCROW_ADDRESS']
 
 ESCROW_LOGICSIG = os.environ['ESCROW_LOGICSIG']
 VALIDATOR_INDEX = int(os.environ['VALIDATOR_INDEX'])
 MANAGER_INDEX = int(os.environ['MANAGER_INDEX'])
+
+TOKEN1_INDEX = int(os.environ['TOKEN1_INDEX'])
+TOKEN2_INDEX = int(os.environ['TOKEN2_INDEX'])
+LIQUIDITY_TOKEN_INDEX = int(os.environ['LIQUIDITY_TOKEN_INDEX'])
 
 algod_client = algod.AlgodClient(ALGOD_TOKEN, ALGOD_ENDPOINT, headers={
   "x-api-key": ALGOD_TOKEN
@@ -34,3 +37,151 @@ def wait_for_transaction(transaction_id):
   assert len(result['transactions']) == 1, result
   return result['transactions'][0]
 
+def add_liquidity_test():
+  print(
+    f"Building add liquidity atomic transaction group..."
+  )
+
+  encoded_app_args = [
+    bytes("a", "utf-8"),
+    bytes("5", "utf-8"),
+  ]
+
+  # Transaction to Validator
+  txn_1 = transaction.ApplicationCallTxn(
+    sender=TEST_ACCOUNT_ADDRESS,
+    sp=algod_client.suggested_params(),
+    index=VALIDATOR_INDEX,
+    on_complete=transaction.OnComplete.NoOpOC,
+    accounts=[ESCROW_ADDRESS, ESCROW_ADDRESS],
+    app_args=encoded_app_args,
+    foreign_assets=[LIQUIDITY_TOKEN_INDEX]
+  )
+
+  # Transaction to Manager
+  txn_2 = transaction.ApplicationCallTxn(
+    sender=TEST_ACCOUNT_ADDRESS,
+    sp=algod_client.suggested_params(),
+    index=MANAGER_INDEX,
+    on_complete=transaction.OnComplete.NoOpOC,
+    accounts=[ESCROW_ADDRESS, ESCROW_ADDRESS],
+    app_args=encoded_app_args,
+    foreign_assets=[LIQUIDITY_TOKEN_INDEX]
+  )
+
+  # Transaction to send Token1 to Escrow
+  txn_3 = transaction.AssetTransferTxn(
+    sender=TEST_ACCOUNT_ADDRESS,
+    sp=algod_client.suggested_params(),
+    receiver=ESCROW_ADDRESS,
+    amt=50,
+    index=TOKEN1_INDEX
+  )
+
+  # Transaction to send Token2 to Escrow
+  txn_4 = transaction.AssetTransferTxn(
+    sender=TEST_ACCOUNT_ADDRESS,
+    sp=algod_client.suggested_params(),
+    receiver=ESCROW_ADDRESS,
+    amt=50,
+    index=TOKEN2_INDEX
+  )
+
+  # Get group ID and assign to transactions
+  gid = transaction.calculate_group_id([txn_1, txn_2, txn_3, txn_4])
+  txn_1.group = gid
+  txn_2.group = gid
+  txn_3.group = gid
+  txn_4.group = gid
+
+  # Sign transactions
+  stxn_1 = txn_1.sign(TEST_ACCOUNT_PRIVATE_KEY)
+  stxn_2 = txn_2.sign(TEST_ACCOUNT_PRIVATE_KEY)
+  stxn_3 = txn_3.sign(TEST_ACCOUNT_PRIVATE_KEY)
+  stxn_4 = txn_4.sign(TEST_ACCOUNT_PRIVATE_KEY)
+
+  # Broadcast the transactions
+  signed_txns = [stxn_1, stxn_2, stxn_3, stxn_4]
+  tx_id = algod_client.send_transactions(signed_txns)
+
+  # Wait for transaction
+  wait_for_transaction(tx_id)
+
+  print(f"Add liquidity transaction sent from User to AlgoSwap successfully! Tx ID: https://testnet.algoexplorer.io/tx/{tx_id}")
+
+  print()
+
+def get_refund_test():
+  print("Attempting to get refund of liquidity tokens from Escrow...")
+
+  encoded_app_args = [
+    bytes("r", "utf-8")
+  ]
+
+  # Transaction to Validator
+  txn_1 = transaction.ApplicationCallTxn(
+    sender=TEST_ACCOUNT_ADDRESS,
+    sp=algod_client.suggested_params(),
+    index=VALIDATOR_INDEX,
+    on_complete=transaction.OnComplete.NoOpOC,
+    accounts=[ESCROW_ADDRESS],
+    app_args=encoded_app_args
+  )
+
+  # Transaction to Manager
+  txn_2 = transaction.ApplicationCallTxn(
+    sender=TEST_ACCOUNT_ADDRESS,
+    sp=algod_client.suggested_params(),
+    index=MANAGER_INDEX,
+    on_complete=transaction.OnComplete.NoOpOC,
+    accounts=[ESCROW_ADDRESS],
+    app_args=encoded_app_args
+  )
+
+  # Calculate unused_liquidity
+  unused_liquidity = 0
+  results = algod_client.account_info(TEST_ACCOUNT_ADDRESS)
+  local_state = results['apps-local-state'][0]
+  for index in local_state:
+    if local_state[index] == MANAGER_INDEX:
+      unused_liquidity = local_state['UL']
+
+  print(f"User unused liquidity is {unused_liquidity}")
+
+  # Transaction to get refund from Escrow
+  program = base64.b64decode(ESCROW_LOGICSIG)
+  lsig = transaction.LogicSig(program)
+
+  txn_3 = transaction.AssetTransferTxn(
+    sender=lsig.address(),
+    sp=algod_client.suggested_params(),
+    receiver=TEST_ACCOUNT_ADDRESS,
+    amt=unused_liquidity,
+    index=LIQUIDITY_TOKEN_INDEX
+  )
+
+  # Get group ID and assign to transactions
+  gid = transaction.calculate_group_id([txn_1, txn_2, txn_3])
+  txn_1.group = gid
+  txn_2.group = gid
+  txn_3.group = gid
+
+  # Sign transactions
+  stxn_1 = txn_1.sign(TEST_ACCOUNT_PRIVATE_KEY)
+  stxn_2 = txn_2.sign(TEST_ACCOUNT_PRIVATE_KEY)
+  stxn_3 = transaction.LogicSigTransaction(txn_3, lsig)
+
+  # Broadcast the transactions
+  signed_txns = [stxn_1, stxn_2, stxn_3]
+  tx_id = algod_client.send_transactions(signed_txns)
+
+  # Wait for transaction
+  wait_for_transaction(tx_id)
+
+  print(f"Got refund of liquidity tokens from AlgoSwap to User successfully! Tx ID: https://testnet.algoexplorer.io/tx/{tx_id}")
+
+  print()
+
+if __name__ == "__main__":
+  add_liquidity_test()
+  get_refund_test()

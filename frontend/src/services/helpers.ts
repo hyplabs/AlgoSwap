a1fcd6f5ffeefa0a3cf5b8989cfa922f5c285ed2
Haardik <hhaardik@uwaterloo.ca>
2021-03-05 13:39:46 -0500

    add helper for swap token1>token2

---
+++ frontend/src/services/helpers.ts
@@ -0,0 +1,13 @@
+export function getToken1Refund() {
+  const encodedAppArgs = new TextEncoder().encode('r');
+
+  console.log('Not implemented');
+}
+
+export function getToken2Refund() {
+  console.log('Not implemented');
+}
+
+export function getLiquidityTokenRefund() {
+  console.log('Not implemented');
+}
